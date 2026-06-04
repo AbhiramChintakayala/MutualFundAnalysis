f7cc9a1d175d26f85af218c48be6c53016b6b6c9
Chintakayala Abhiram <chintakayalaabhiram@gmail.com>
2026-06-04 21:19:03 +0530

    Updated API and validation code

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -29,3 +29,30 @@ for file in csv_files:
 
     print("\nFirst 5 Rows:")
     print(df.head())
+
+# fund_master = pd.read_csv("data/raw/01_fund_master.csv")
+
+# print("Fund Houses:")
+# print(fund_master["fund_house"].unique())
+
+# print("\nCategories:")
+# print(fund_master["category"].unique())
+
+# print("\nSub Categories:")
+# print(fund_master["sub_category"].unique())
+
+# print("\nRisk Grades:")
+# print(fund_master["risk_category"].unique())
+
+
+
+# fund_master = pd.read_csv("data/raw/01_fund_master.csv")
+# nav_history = pd.read_csv("data/raw/02_nav_history.csv")
+
+# fund_codes = set(fund_master["amfi_code"])
+# nav_codes = set(nav_history["amfi_code"])
+
+# missing_codes = fund_codes - nav_codes
+
+# print("Missing Codes:", len(missing_codes))
+# print(missing_codes)
